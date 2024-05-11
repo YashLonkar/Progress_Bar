dc88376d1deda92b8c3544dcd921fc6aadcd627c
YashLonkar <yashlonkar99@gmail.com>
2024-05-11 17:19:25 +0530

    Added Accessibilty, UI Improve

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ProgressBar from './components/ProgressBar'
 import { useEffect } from 'react'
-import StartStop from './components/StartStop'
+// import StartStop from './components/StartStop'
 
 function App() {
   const [Value, setValue] = useState(0);
+  const [success, setsuccess] = useState(false);
 
   useEffect(() =>{
     setInterval(()=>{
-      setValue((val)=> val+1);
-    },100);
+      setValue((val)=> val+0.1);
+    },20);
   },[]);
   
   return (
   
     <div className='App'>
       <span>Progress Bar</span>
-      <ProgressBar Value={Value}/> 
-      <div className='btn'>
-      <button className='btn'>Start</button>
-      <button className='btn'>Stop</button>
-      </div>
+      <ProgressBar Value={Value}
+      onComplete={() => setsuccess(true)}
+      /> 
+      <span>{success?"Complete!":"Loading..."}</span>
+      
     </div>
     
     
-  )
+  );
 }
 
 export default App
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 import { useEffect,useState } from 'react';
 import './progressBar.css';
-function ProgressBar({Value=0}) {
+import {MAX,MIN} from '../constants';
+function ProgressBar({Value=0, onComplete=()=> {}}) {
 
   const [Percent, setPercent] = useState(Value);
-
+  
   useEffect(()=> {
-    setPercent(Math.min(100,Math.max(Value,0)));
+    setPercent(Math.min(MAX,Math.max(Value,MIN)));
+
+    if(Value>=MAX){
+      onComplete();
+    }
   },[Value]);
 
   return (
     <div className='Progress'>
         <span
-        style={{color:Percent > 49?"white":"black"}}>{Percent.toFixed()}%</span>
-        <div className='loading' style={{width: `${Percent}%`}}></div>
+        style={{
+          color:Percent > 49?"white":"black"
+          }}>
+          {Percent.toFixed()}%
+          </span>
+        <div className='loading' 
+        // style={{width: `${Percent}%`}}  //Instead of this we can use scale to increase the performance
+          style={{
+          transform:`scaleX(${Percent/MAX})`,
+          transformOrigin:"left"
+          }}
+          role="progressbar"
+          aria-valuemin={MIN}
+          aria-valuemax={MAX}
+          aria-valuenow={Percent}
+        />
     </div>
   ) 
 }
